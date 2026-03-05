58e9fb5266ba489e891224e0bb37d8dc2ec5d05f
muhammad-ahmed43432 <55586@students.riphah.edu.pk>
2026-03-04 23:38:06 -0800

    Add files via upload

---
+++ mapping.py
@@ -0,0 +1,12 @@
+symptom_to_deficiency = {
+    "angular_cheilitis": "Vitamin B12",
+    "bitot_spots": "Vitamin A",
+    "glossitis": "Vitamin B12",
+    "healthy_elbows": "Healthy",
+    "healthy_eye": "Healthy",
+    "healthy_lips": "Healthy",
+    "healthy_mouth": "Healthy",
+    "healthy_tongue": "Healthy",
+    "phrynoderma": "Vitamin A",
+    "ulcer": "Vitamin B12"
+}
